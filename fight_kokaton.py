48f53e45065a99a92fab499de9bcd745fbee2e1f
C0C2504030 <C0C2504030@edu.teu.ac.jp>
2026-04-28 14:34:47 +0900

    練習3：こうかとんは喜んだ

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,9 @@ def main():
                 if beam.rct.colliderect(bomb.rct): #練習2　ビームと爆弾の衝突
                     beam = None  
                     bomb = None
+                    bird.change_img(6, screen) #練習3 こうかとん喜びの舞
+                    pg.display.update()
+                    time.sleep(1)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
